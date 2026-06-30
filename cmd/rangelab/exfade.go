8450be8b7e02e146d4a17cc4a594c564fdc09c61
laclance <laclance@gmail.com>
2026-06-30 17:18:37 +0200

    Add exhaustion fade CLI header

--- cmd/rangelab/exfade.go
+++ cmd/rangelab/exfade.go
@@ -1,1 +1,34 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"strings"
+
+	"range-strategy-lab/internal/lab"
+)
+
+const edgeFadeFlagName = "backtest-first-btc-15m-range-edge-exhaustion-fade-v1"
+
+func init() {
+	if !edgeFadeFlagPresent(os.Args[1:]) {
+		return
+	}
+	if err := runEdgeFadeWithArgs(os.Args[1:]); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+func edgeFadeFlagPresent(args []string) bool {
+	prefix := "-" + edgeFadeFlagName
+	for _, arg := range args {
+		if arg == prefix || strings.HasPrefix(arg, prefix+"=") {
+			return true
+		}
+	}
+	return false
+}
