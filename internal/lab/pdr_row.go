a333953c3fdabc18a4ee9484cd3a9ee447d85d8a
laclance <laclance@gmail.com>
2026-06-30 16:08:48 +0200

    Make pdr signal ids unique

--- internal/lab/pdr_row.go
+++ internal/lab/pdr_row.go
@@ -1,10 +1,13 @@
 package lab
 
+import "strconv"
+
 func (s *BTC15MPreviousDayRangeReversionStrategy) btc15MPrevDaySignalRow(candles []Candle, d int, split string, priorKey string, prior btc15MDayRange, side Direction) BTC15MPreviousDayRangeReversionSignalRow {
 	priorATR := s.atr[d-1]
 	stop := prior.low - s.cfg.StopATRMultiple*priorATR
 	if side == Short {
 		stop = prior.high + s.cfg.StopATRMultiple*priorATR
 	}
-	return BTC15MPreviousDayRangeReversionSignalRow{SignalID: BTC15MPreviousDayRangeReversionCandidateID, CandidateID: BTC15MPreviousDayRangeReversionCandidateID, Split: split, DecisionIndex: d, DecisionCloseTime: candles[d].CloseTime.UTC().Format(timeLayout), DecisionClose: candles[d].Close, Side: side, TimingLabel: "next_15m_open", PriorDay: priorKey, PriorDayHigh: prior.high, PriorDayLow: prior.low, PriorDayMidpoint: prior.mid, PriorDayRangeWidth: prior.width, OuterZonePct: s.cfg.OuterZonePct, PriorATR14: priorATR, Stop: stop, Target: prior.mid, MaxHoldBars: s.cfg.MaxHoldBars}
+	signalID := BTC15MPreviousDayRangeReversionCandidateID + "_" + strconv.Itoa(len(s.rows)+1)
+	return BTC15MPreviousDayRangeReversionSignalRow{SignalID: signalID, CandidateID: BTC15MPreviousDayRangeReversionCandidateID, Split: split, DecisionIndex: d, DecisionCloseTime: candles[d].CloseTime.UTC().Format(timeLayout), DecisionClose: candles[d].Close, Side: side, TimingLabel: "next_15m_open", PriorDay: priorKey, PriorDayHigh: prior.high, PriorDayLow: prior.low, PriorDayMidpoint: prior.mid, PriorDayRangeWidth: prior.width, OuterZonePct: s.cfg.OuterZonePct, PriorATR14: priorATR, Stop: stop, Target: prior.mid, MaxHoldBars: s.cfg.MaxHoldBars}
 }
